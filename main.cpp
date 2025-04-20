03008d1f9d66cd0c311a81cea5a9c7307449be6a
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2025-04-20 15:29:04 +0300

    had to flip the values that the IK works with.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <cmath>
 #include <array>
@@ -79,32 +80,35 @@ struct Point globalToLocal(struct Point globalFootPos, double baseX, double base
 }
 
 struct Angles posToAngle(struct Point& p) {
-    double x = p.x;
-    double y = p.y;
-    double z = p.z;
+    double x = -p.x;
+    double y = -p.y;
+    double z = -p.z;
 
+    // Apply rest offsets
     y += YRest;
     z += ZRest;
 
-    // CALCULATE INVERSE KINEMATIC SOLUTION
-    double J1 = atan2(x, y) * (180 / PI);
-    double H = sqrt((y * y) + (x * x));
-    double L = sqrt((H * H) + (z * z));
-    double J3 = acos(   ((J2L * J2L) + (J3L * J3L) - (L * L))   /   (2 * J2L * J3L)   ) * (180 / PI);
-    double B = acos(   ((L * L) + (J2L * J2L) - (J3L * J3L))   /   (2 * L * J2L)   ) * (180 / PI);
-    double A = atan2(z, H) * (180 / PI);  // BECAUSE Z REST IS NEGATIVE, THIS RETURNS A NEGATIVE VALUE
-    double J2 = (B + A);  // BECAUSE 'A' IS NEGATIVE AT REST WE NEED TO INVERT '-' TO '+'
+    // Correctly handle quadrant: atan2 avoids flipping
+    double J1 = atan2(x, y) * (180.0 / PI);
 
-    /*
-    cout << 90 - J1 << endl;
-    cout << 90 + J2 << endl;
-    cout << J3 + 2 << endl;
-    */
+    double H = sqrt(x * x + y * y);
+    double L = sqrt(H * H + z * z);
+
+    double cosJ3 = ((J2L * J2L) + (J3L * J3L) - (L * L)) / (2.0 * J2L * J3L);
+    cosJ3 = std::clamp(cosJ3, -1.0, 1.0); // prevent domain errors
+    double J3 = acos(cosJ3) * (180.0 / PI);
+
+    double cosB = ((L * L) + (J2L * J2L) - (J3L * J3L)) / (2.0 * L * J2L);
+    cosB = std::clamp(cosB, -1.0, 1.0);
+    double B = acos(cosB) * (180.0 / PI);
+
+    double A = atan2(z, H) * (180.0 / PI);
+    double J2 = B + A;
 
     struct Angles a;
-    a.J1 = 90 - J1;
-    a.J2 = 90 + J2;
-    a.J3 = J3 + 2;
+    a.J1 = 90.0 - J1;  // Turning toward X axis
+    a.J2 = 90.0 + J2;  // Up/down
+    a.J3 = J3 + 2.0;   // Joint compensation offset
     return a;
 }
 
@@ -170,7 +174,7 @@ int main() {
 
     struct Point point;
     point.x = 0;
-    point.y = 40;
+    point.y = -40;
     point.z = 0;
     struct Angles a = posToAngle(point);
     openSerialPort("/dev/ttyUSB0", 115200);
