5a8c53ab259ccf1329345b2e1e6e49964a22a7d6
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2025-04-20 12:21:29 +0300

    made inverse kinematics

--- main.cpp
+++ main.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
+#include <cmath>
+#include <array>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+#define J2L 58
+#define J3L 138
+#define YRest 58
+#define ZRest -138
+#define PI 3.14159265
+
+using namespace std;
+
+struct Point {
+    double x;
+    double y;
+    double z;
+};
+
+void posToAngle(double x, double y, double z) {
+    y += YRest;
+    z += ZRest;
+
+    // CALCULATE INVERSE KINEMATIC SOLUTION
+    double J1 = atan(x / y) * (180 / PI);
+    double H = sqrt((y * y) + (x * x));
+    double L = sqrt((H * H) + (z * z));
+    double J3 = acos(   ((J2L * J2L) + (J3L * J3L) - (L * L))   /   (2 * J2L * J3L)   ) * (180 / PI);
+    double B = acos(   ((L * L) + (J2L * J2L) - (J3L * J3L))   /   (2 * L * J2L)   ) * (180 / PI);
+    double A = atan(z / H) * (180 / PI);  // BECAUSE Z REST IS NEGATIVE, THIS RETURNS A NEGATIVE VALUE
+    double J2 = (B + A);  // BECAUSE 'A' IS NEGATIVE AT REST WE NEED TO INVERT '-' TO '+'
+
+    cout << 90 - J1 << endl;
+    cout << 90 + J2 << endl;
+    cout << J3 + 2 << endl;
+
+}
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    struct Point point;
+    point.x = 0;
+    point.y = 0;
+    point.z = 0;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
+    posToAngle(point.x, point.y, point.z);
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
