6b410ceaccdef66d399c92df42f5ad4508decca9
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2025-04-22 21:45:31 +0300

    separated serial to its own files

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@
 #define ZRest -138
 #define PI 3.14159265
 
+#include "Serial.h"
+
 using namespace std;
 
 struct Point {
@@ -26,46 +28,6 @@ struct Angles {
     double J2;
     double J3;
 }Angles;
-
-int serialPortFD = -1;
-
-void openSerialPort(const char* port, int baudRate) {
-    serialPortFD = open(port, O_RDWR | O_NOCTTY | O_NDELAY);
-
-    if (serialPortFD == -1) {
-        std::cerr << "Failed to open port: " << port << std::endl;
-        return;
-    }
-
-    termios options;
-    tcgetattr(serialPortFD, &options);
-
-    cfsetispeed(&options, baudRate);
-    cfsetospeed(&options, baudRate);
-
-    options.c_cflag |= (CLOCAL | CREAD);
-    options.c_cflag &= ~PARENB;
-    options.c_cflag &= ~CSTOPB;
-    options.c_cflag &= ~CSIZE;
-    options.c_cflag |= CS8;
-    options.c_cflag &= ~CRTSCTS;
-
-    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-    options.c_iflag &= ~(IXON | IXOFF | IXANY);
-    options.c_oflag &= ~OPOST;
-
-    tcsetattr(serialPortFD, TCSANOW, &options);
-
-    std::cout << "Serial port opened: " << port << std::endl;
-}
-
-void closeSerialPort() {
-    if (serialPortFD != -1) {
-        close(serialPortFD);
-        std::cout << "Serial port closed." << std::endl;
-        serialPortFD = -1;
-    }
-}
 
 // Transforms global (robot-centered) foot position into leg-local position
 struct Point globalToLocal(struct Point globalFootPos, double baseX, double baseY) {
